08c641a61977d21a7e81beb1e33e97899a91ed77
ShonTechDev <shonta.pierce@gmail.com>
2026-04-27 23:59:47 -0400

    Lesson 03 CTD Swag:added cart functionality with event handling and useEffect/implemented cart state, add-to-cart handler and header updates

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
 import ctdLogo from './assets/mono-blue-logo.png';
 
-function Header() {
+function Header({ cart }) {
+  useEffect(() => {
+    cart.forEach((item) => {
+      console.log(item.baseName, item.cartItemId);
+    });
+
+    if (cart.length > 0) {
+      console.log('--end of cart--');
+    }
+  });
   return (
     <div className="coming-soon">
       <h1>CTD Swag</h1>
+
+      <p>Cart: {cart.length}</p>
+
       <div style={{ height: 200, width: 100, margin: '0 auto' }}>
         <img src={ctdLogo} alt="Code The Dream Logo" />
       </div>
